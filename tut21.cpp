4dd0e2bfcb3b4da9b64d714c34d7576af533e594
Lokesh <lokesh_repaka@srmap.edu.in>
2021-06-12 18:01:57 +0530

    secound commit

--- tut21.cpp
+++ tut21.cpp
@@ -11,11 +11,11 @@ public:
 
     void setData(int a, int b, int c);
     void getData(){
-        cout<<"The value of a is "<<a <<endl;
-        cout<<"The value of b is "<<b <<endl;
-        cout<<"The value of c is "<<c <<endl;
-        cout<<"The value of d is "<<d <<endl;
-        cout<<"The value of e is "<<e <<endl;
+        cout<<"The value of a is "<<a  <<endl;
+        cout<<"The value of b is "<<b  <<endl;
+        cout<<"The value of c is "<<c  <<endl;
+        cout<<"The value of d is "<<d  <<endl;
+        cout<<"The value of e is "<<e  <<endl;
     }
 };
 
